04d26efe4a403b561b764125173fd85bd4f85f9e
Emanuel Rista <e.rista@outlook.it>
2021-04-12 20:30:45 +0200

    Angular Router Basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from '../app/menu/menu.component';
+import { DishdetailComponent } from '../app/dishdetail/dishdetail.component';
+import { HomeComponent } from '../app/home/home.component';
+import { AboutComponent } from '../app/about/about.component';
+import { ContactComponent } from '../app/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
